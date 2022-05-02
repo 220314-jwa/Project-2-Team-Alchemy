32b4f3ffb1759bf930e38925f7bf6f48cfa6bb32
timmy.wang777 <timmy.wang777@gmail.com>
2022-05-02 09:14:41 -0700

    finish merging with Monica

--- src/main/Project-2-Team-Alchemy-Front/src/app/app-routing.module.ts
+++ src/main/Project-2-Team-Alchemy-Front/src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeImgTittleComponent } from './home-img-tittle/home-img-tittle.component';
+import { RestaurantListAndReviewComponent } from './restaurant-list-and-review/restaurant-list-and-review.component';
 
 const routes: Routes = [
-  
-
+  {
+    path:'',
+    component: HomeImgTittleComponent
+  },
+  {
+    path:'home',
+    component: HomeImgTittleComponent
+  },
+  {
+    path:'restaurant-list',
+    component:RestaurantListAndReviewComponent
+  }
 
 
 
