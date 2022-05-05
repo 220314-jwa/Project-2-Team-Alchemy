e1955b2b4b2913fd3670d5e9d55e9364047a5d8e
timmy.wang777 <timmy.wang777@gmail.com>
2022-05-05 09:32:43 -0700

    commit

--- src/main/java/com/revature/sierra/alchemy/MVC/Controllers/UsersController.java
+++ src/main/java/com/revature/sierra/alchemy/MVC/Controllers/UsersController.java
@@ -1,5 +1,32 @@
 package com.revature.sierra.alchemy.MVC.Controllers;
 
-public class UsersController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(path="/users") 
+@CrossOrigin(origins="http://localhost:4200") 
+public class UsersController {
+	
+	@GetMapping
+	public ResponseEntity<Integer> getUser(){
+		Integer a = 1;
+		return ResponseEntity.ok(a);
+	}
+	
+	@PutMapping(path="/{userID}")
+	public ResponseEntity<Integer> getUserById(@PathVariable int userID){
+		return ResponseEntity.ok(1);
+	}
 }
