28d395a4a464cfeeffc3187955b0e39930ec00d8
timmy.wang777 <timmy.wang777@gmail.com>
2022-05-11 12:25:13 -0700

    add restaurant service

--- src/main/java/com/revature/sierra/alchemy/MVC/Service/ReviewServiceImpl.java
+++ src/main/java/com/revature/sierra/alchemy/MVC/Service/ReviewServiceImpl.java
@@ -2,10 +2,17 @@ package com.revature.sierra.alchemy.MVC.Service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.revature.sierra.alchemy.MVC.Models.Reviews;
 
+@Service
 public class ReviewServiceImpl implements ReviewService {
 
+	public ReviewServiceImpl() {
+		
+	}
+	
 	@Override
 	public Reviews create(Reviews newReviews) {
 		// TODO Auto-generated method stub
